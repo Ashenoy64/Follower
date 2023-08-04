0c04c297daa13f231a29a7fbf460ec94ad683eea
Meowmaster <ashenoy64@gmail.com>
2023-08-04 12:49:57 +0530

    renamed 2 files and add readme

--- README.md
+++ README.md
@@ -1,6 +1,45 @@
 # Follower
-This uses instaloader Library to get list of people who dont follow you back on Instagram.<br>
-In order to run this you have to add some default instagram userid and password in to the code. This is to get info of public Instagram account.
 
-Currently it dosent have the functionality to send the list to the given email.
+Follower is a Python project that helps you identify the users on Instagram whom you follow, but they don't follow you back. It utilizes the `instaloader` module to fetch the necessary data from Instagram. The project includes two separate applications one built with Tkinter and another with Kivy for user interaction.
 
+## Installation
+
+To use the Follower project, follow these steps:
+
+1. Clone the project from the GitHub repository or download the zip file and extract it:
+
+   ```
+   git clone https://github.com/Ashenoy64/Follower.git
+   ```
+
+2. Install the required dependencies:
+
+   ```
+   pip install -r requirements.txt
+   ```
+
+## How to Use
+
+To run the Tkinter app:
+
+```
+python tkinter_app.py
+```
+
+This will launch the Tkinter application, where you can log in with your Instagram credentials and get the list of users who don't follow you back.
+
+To run the Kivy app:
+
+```
+python kivy_app.py
+```
+
+The Kivy application provides a user-friendly interface to perform the same functionality as the Tkinter app.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Disclaimer
+
+Please note that this project is for educational and personal use only. It interacts with Instagram's API, so ensure you comply with Instagram's terms of service while using this project. The developers are not responsible for any misuse of this tool.
---
+++ tkinter_app.py
@@ -0,0 +1,102 @@
+from tkinter import *
+import instaloader
+master=Tk()
+#instaloader is not working
+master.title("Following Back")
+master.geometry("500x300")
+    
+
+def search():
+    notice.config(text="")
+    global body
+    if username.get()=="":
+        notice.config(text="Username must not be Empty")
+        return 
+    elif password=="":
+        notice.config(text="Password shoudnt be Empty")
+        return
+    
+    
+    loader=instaloader.Instaloader()
+        
+    try:
+        loader.login(username.get(),password.get())  
+    except:
+        notice.config(text="Login Failed")
+        return
+    
+    notice.config(text="Login Successfull")
+    
+    try:
+        profile=instaloader.Profile.from_username(loader.context,username.get())
+    except:
+        notice.config(text="Failed to get list of followers and followings")
+        return
+    followers_iterator=profile.get_followers()
+    followers=set()
+
+    for f in followers_iterator:
+        followers.add(f.username)
+
+    notice.config(text="Obtained List of followers")
+
+    following=set()
+    for i in profile.get_followees():
+        following.add(i.username)
+    notice.config(text="Obtained List of followings")
+
+    not_following=following-followers
+    number_of_not_following=len(not_following)
+
+    for i in not_following:
+        body = body + i + "\n"
+
+    loader.close()
+    notice.config(text="")
+
+    body=body+"No of People:"+str(number_of_not_following)    
+
+
+    Result=Toplevel(master)
+    Result.title('Result')
+    out=Label(Result,text=body,padx="2",pady="2",width="50")
+    out.pack()
+
+#All the widets
+dumy_label=Label(master,padx="2",pady="2") 
+
+details_label=Label(master,text="Please Fill the below Details",padx="2",pady="2")  
+
+username_label=Label(master,text="Enter your Instagram username",padx="2",pady="2")     
+username=Entry(master,) 
+
+password_label=Label(master,text="Enter Your Instagram Password",padx="2",pady="2")  
+password=Entry(master,show="*",) 
+
+
+
+search=Button(master,text="Find",command=search,padx="2",pady="2")
+
+notice=Label(master,fg="red",padx="2",pady="2")
+
+
+#Placement of the widgets
+
+dumy_label.grid(column=0,row=0)
+
+details_label.grid(column=0,row=1)
+
+
+username_label.grid(column=0,row=4,sticky=W)
+username.grid(column=2,row=4,sticky=E)
+
+password_label.grid(column=0,row=5,sticky=W)
+password.grid(column=2,row=5)
+
+
+search.grid(row=10,columnspan=2,sticky=E)
+
+notice.grid(row=11,columnspan=2)
+
+
+master.mainloop()
